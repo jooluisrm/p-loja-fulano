5023affabc304fd180ade44523b148c9c0d0ab80
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-07-02 10:39:50 -0300

    Projeto: Loja do Fulano - Primeira Tela

---
+++ components/button.tsx
@@ -0,0 +1,30 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type Props = {
+    title: string;
+    onPress: () => void;
+}
+
+export const Button = ({ title, onPress }: Props) => {
+    return (
+        <Pressable onPress={onPress} style={styles.button}>
+                <Text style={styles.buttonText}>
+                    {title}
+                </Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "#164E85",
+        borderRadius: 10,
+        paddingVertical: 15,
+        paddingHorizontal: 25
+    },
+    buttonText: {
+        fontSize: 16,
+        color: "#fff",
+        textAlign: "center"
+    }
+});
